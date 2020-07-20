0ca3068cd4003e6d1c2edd69d1b6136597a4a3a0
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-20 11:53:50 -0700

    Acme Accounting Step 54

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remeber, we're \"accounting\" on you!");
+        Console.ReadLine();
+
+    }
+}
