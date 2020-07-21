d8a74959f47c8d7b535f3c7fbf50d3b02ab0369a
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-21 12:42:57 -0700

    Boolean Logic Car Insurance Step 100

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Age;
+            Console.WriteLine("What is your age?");
+            Age = Console.ReadLine();
+            int age = Convert.ToInt32(Age);
+
+            string DUI;
+            Console.WriteLine("Have you ever had a DUI?  Please answer \"yes\" or \"no\".");
+            DUI = Console.ReadLine();
+
+            string Tickets;
+            Console.WriteLine("How many speeding tickets do you have?");
+            Tickets = Console.ReadLine();
+            int speedTickets = Convert.ToInt32(Tickets);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && DUI == "no" && speedTickets <= 3);
+            Console.ReadLine();
+        }
+    }
+}
