1805c6843ee7863d2f017f2ce73a3510c555aac7
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-22 15:27:01 -0700

    WhileDoWhileLoops Step 121

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many times do you think you'll need to press enter until this while loop ends? \nIt's less than 100!  haha!");     
+            {
+                int i = 0;
+                while (i <= 10)
+                {
+                    Console.WriteLine(i);
+                    Console.ReadLine();
+                    i++;
+                }
+
+                Console.WriteLine("Now that wasn't too bad, was it?"); 
+                Console.WriteLine("Press Enter Key continue to the do while statement.");
+                Console.ReadLine();
+
+
+                Console.WriteLine("What letter does my first name start with? (Please type capital letter)");
+                string fName = Console.ReadLine();
+
+                bool isGuess = fName == "L";
+
+                do
+                {
+                    switch (fName)
+                    {
+                        case "J":
+                            Console.WriteLine("You're very warm!!  Keep guessing!");
+                            fName = Console.ReadLine();
+                            break;
+                        case "N":
+                            Console.WriteLine("You're very warm!!  Keep guessing!");
+                            fName = Console.ReadLine();
+                            break;
+                        case "K":
+                            Console.WriteLine("Oh you are sooooo close!!  Keep guessing!");
+                            fName = Console.ReadLine();
+                            break;
+                        case "M":
+                            Console.WriteLine("Oh you are sooooo close!!  Keep guessing!");
+                            fName = Console.ReadLine();
+                            break;
+                        case "L":
+                            Console.WriteLine("Ding! Ding! Ding!  You are the winner! \nThanks for playing!");
+                            isGuess = true;
+                            break;
+                        default:
+                            Console.WriteLine("You are way off!  Try again");
+                            fName = Console.ReadLine();
+                            break;
+                    }
+                }
+                while (!isGuess);
+
+                Console.Read();
+
+            }
+
+            
+        }
+    }
+}
