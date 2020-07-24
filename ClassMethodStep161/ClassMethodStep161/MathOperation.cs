945f2db89a24f74c54cc8dae910c0d53a4f8132f
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-24 15:27:57 -0700

    ClassMethod Step 161

---
+++ ClassMethodStep161/ClassMethodStep161/MathOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodStep161
+{
+    class MathOperation
+    {
+        public static int Add(int num)
+        {
+            return num + 2;
+        }
+        public static int Mult(int num)
+        {
+            return num * 2;
+        }
+        public static int Subtract(int num)
+        {
+            return num - 2;
+        }
+        public static int Divide(int num)
+        {
+            return num / 2;
+        }
+    }
+}
---
+++ ClassMethodStep161/ClassMethodStep161/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodStep161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Enter a number and the program will perform 4 math operations on that number.");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num + " + 2 = " + MathOperation.Add(num));
+                Console.WriteLine(num + " x 2 = " + MathOperation.Mult(num));
+                Console.WriteLine(num + " - 2 = " + MathOperation.Subtract(num));
+                Console.WriteLine(num + " / 2 = " + MathOperation.Divide(num));
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
