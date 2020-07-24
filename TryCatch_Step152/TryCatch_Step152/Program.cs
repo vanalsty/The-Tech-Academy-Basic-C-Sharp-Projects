52d7b605dbdffa858433c4c1bfb0bf64a68095f6
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-24 11:23:29 -0700

    TryCatch Step 152

---
+++ TryCatch_Step152/TryCatch_Step152/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch_Step152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int>() { 2, 4, 6, 8, 16, 32, 64, 128 };
+
+            try
+            {
+                Console.WriteLine("Please give a number and this program will divide each number in the list by the number entered.");
+                int div1 = Convert.ToInt32(Console.ReadLine());
+
+
+                for (int i = 0; i < numList.Count; i++)
+                {
+                    int result = numList[i] / div1;
+                    Console.WriteLine(numList[i] + " divided by " + div1 + " = " + result);
+
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+
+
+}
+ 
