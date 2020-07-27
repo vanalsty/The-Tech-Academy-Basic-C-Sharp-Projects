ecb632598d4bee2254cd8df3be4226159ab1fd04
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-27 13:57:22 -0700

    ClassMethod Step 165

---
+++ ClassMethodStep165/ClassMethodStep165/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodStep165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chose a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Chose a second number. Press return if you do not want a second number:");
+            string answer = Console.ReadLine();
+
+            MathOps FirstOp = new MathOps();
+
+            Console.WriteLine("Your number(s) + 10 = " + FirstOp.MathMethod(num1, answer));
+
+            Console.ReadLine();
+
+        }
+    }
+}
