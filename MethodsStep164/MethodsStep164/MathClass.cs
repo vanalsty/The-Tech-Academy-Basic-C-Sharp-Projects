afd091e648a602a9ff06d54eb5fe532b3fafb146
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-27 13:36:07 -0700

    ClassMethods Step 164

---
+++ MethodsStep164/MethodsStep164/MathClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsStep164
+{
+    class MathClass
+    {
+        public static int Multiply(int x, int y)
+        {
+            int result = x * y;
+            return result;
+        }
+
+        public static decimal Add(decimal x, int y)
+        {
+            decimal result = (int)(x + y);
+            return (decimal)result;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            int result = x / y;
+            return result;
+        }
+    }
+}
---
+++ MethodsStep164/MethodsStep164/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsStep164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be multiplied by 2:");
+            string myInt = Console.ReadLine();
+            int y = Convert.ToInt32(myInt);
+            Console.WriteLine(MathClass.Multiply(2, y));
+            Console.ReadLine();
+
+
+            Console.WriteLine("Enter a decimal number and 2 will be added to it and an integer returned: ");
+            string input = Console.ReadLine();
+            decimal userDec = Convert.ToDecimal(input);
+            Console.WriteLine(MathClass.Add(userDec, 2));
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number to be divided by 10");
+            string userString = Console.ReadLine();
+            int userInt = Convert.ToInt32(userString);
+            Console.WriteLine(MathClass.Divide(userInt, 10));
+            Console.ReadLine();
+        }
+    }
+}
