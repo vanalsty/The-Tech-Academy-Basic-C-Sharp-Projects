d74347bdc2f3d2e540c69d9cc1f9aed97abb01cb
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-28 15:35:07 -0700

    AbstractClass Step 179

---
+++ AbstractClassStep179/AbstractClassStep179/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassStep179
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public abstract void SayName();
+    }
+}
---
+++ AbstractClassStep179/AbstractClassStep179/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassStep179
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee name = new Employee()
+                {
+                    firstName = " Sample",
+                    lastName = " Student",
+                };
+                name.SayName();
+                Console.ReadLine();
+        }
+    }
+}
