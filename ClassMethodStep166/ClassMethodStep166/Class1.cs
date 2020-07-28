29b7ec0251582730080f29237a390f8f9ac5c2be
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-28 07:02:33 -0700

    ClassMethod Step 166

---
+++ ClassMethodStep166/ClassMethodStep166/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodStep166
+{
+    class Class1
+    {
+        public void MyMethod(int int1, int int2)
+        {
+            int result = int1 * 2;
+            Console.WriteLine(int1 + " multiplied by 2 = " + result);
+            Console.WriteLine(int2);
+            return;
+        }
+    }
+}
---
+++ ClassMethodStep166/ClassMethodStep166/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodStep166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 mathObject = new Class1();
+            Console.WriteLine("Enter an integer: ");
+            string userInput = Console.ReadLine();
+            int int1 = Convert.ToInt32(userInput);
+            Console.WriteLine("Enter a different integer: ");
+            string userInput2 = Console.ReadLine();
+            int int2 = Convert.ToInt32(userInput2);
+
+            mathObject.MyMethod(int1, int2);
+            Console.ReadLine();
+        }
+    }
+}
