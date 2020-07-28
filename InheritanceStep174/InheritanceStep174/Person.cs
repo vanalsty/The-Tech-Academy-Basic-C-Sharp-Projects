ea744e62d214aa276e2806f99625647ecb00ea71
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-28 14:05:36 -0700

    Inheritance Step 174

---
+++ InheritanceStep174/InheritanceStep174/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceStep174
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public void SayName()
+        {
+            Console.WriteLine("Name:" + FirstName + LastName);
+        }
+    }
+}
---
+++ InheritanceStep174/InheritanceStep174/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceStep174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee name = new Employee()
+            {
+                FirstName = " Sample",
+                LastName = " Student",
+            };             
+            name.SayName();
+            Console.ReadLine();
+        }
+    }
+}
