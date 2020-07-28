4ae36788698433958226042bb2de381fd9b41ba7
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-28 11:47:12 -0700

    ClassMethod Step 169

---
+++ ClassMethodStep169/ClassMethodStep169/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodStep169
+{
+    class Class2
+    {
+        public static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ClassMethodStep169/ClassMethodStep169/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodStep169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 numbers = new Class1();
+
+            Console.WriteLine("Enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            numbers.Num1(out int Number1);
+            int result1 = userNum / Number1;
+            
+            Console.WriteLine(userNum + " divided by 2 = " + result1);
+            Console.ReadLine();
+        
+        }
+    }
+}
