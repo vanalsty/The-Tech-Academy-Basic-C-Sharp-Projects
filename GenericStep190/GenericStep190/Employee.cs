501a62372682ce3088c775fbf2bc1f7b91bc7cd6
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-29 15:37:11 -0700

    Generic Step190

---
+++ GenericStep190/GenericStep190/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStep190
+{
+    class Employee<T>
+    {
+        public List<T> things { get; set; }
+    }
+}
+
---
+++ GenericStep190/GenericStep190/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStep190
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee<string> employeesStr = new Employee<string>();
+            employeesStr.things = new List<string>();
+            employeesStr.things.Add("Jack McFarland");
+            employeesStr.things.Add("Grace Adler");
+            employeesStr.things.Add("Will Truman");
+            employeesStr.things.Add("Karen Walker");
+
+
+            Employee<int> employeesInt = new Employee<int>();
+            employeesInt.things = new List<int>();
+            employeesInt.things.Add(1234);
+            employeesInt.things.Add(1235);
+            employeesInt.things.Add(1236);
+            employeesInt.things.Add(1237);
+
+
+            foreach (var str in employeesStr.things)
+                Console.WriteLine("{0}", str);
+
+            
+            foreach (int integer in employeesInt.things)
+                Console.WriteLine("{0}", integer);
+
+            Console.ReadLine();
+        }
+    }
+}
