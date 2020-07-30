d43430990a51473d4fe1d6d7ff9c4356d93c7ec7
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-30 07:21:39 -0700

    Enums Step 195

---
+++ EnumsStep195/EnumsStep195/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsStep195
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the day of the week: ");
+
+            try
+            {
+                DaysOfTheWeek daySelect = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+                foreach (string day in Enum.GetNames(typeof(DaysOfTheWeek)))
+                {
+                    Console.WriteLine("{0} = {1:D}", day, Enum.Parse(typeof(DaysOfTheWeek), day));
+
+                }
+                Console.WriteLine("You entered: " + "{0} = {1:D}", daySelect, Enum.Parse(typeof(DaysOfTheWeek), daySelect.ToString()));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week: ");
+            }
+            Console.ReadLine();
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+    }
+
