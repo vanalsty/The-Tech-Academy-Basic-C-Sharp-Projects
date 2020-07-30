53cb65753dc23372ee7e333bb14524af02597998
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-30 08:23:01 -0700

    Structs Step200

---
+++ StructsStep200/StructsStep200/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsStep200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 88.4m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+
+        
+    }
+}
