e8b6eba27b82986fb6070112444fc0873b8e8956
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-31 09:47:29 -0700

    DateTimeStep222

---
+++ DateTimeStep222/DateTimeStep222/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeStep222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            
+            Console.WriteLine("Please enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            DateTime current = DateTime.Now;
+            var future = current.AddHours(num);
+
+            Console.WriteLine("It will be " + future + " in " + num + " hours");
+            Console.ReadLine();
+        }
+    }
+}
