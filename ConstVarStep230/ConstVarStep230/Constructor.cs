84d98c666d151c3fce5353cd6adeaf6d4c9b478e
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-31 14:10:59 -0700

    ConstVarStep230

---
+++ ConstVarStep230/ConstVarStep230/Constructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarStep230
+{
+    public class Constructor
+    {
+        public Constructor(string name) : this(name, 10)
+        { 
+        }
+
+        public Constructor(string name, int dogAge)
+        {
+            Name = name;
+            Age = dogAge;
+
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+    
+
+}
+
---
+++ ConstVarStep230/ConstVarStep230/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarStep230
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const string showDogName = "Barbary Bow Chicka Wow Wow";
+            Console.WriteLine("Show dogs have the craziest names!\r\nHere's an example: {0}! ", showDogName);
+            var pupAge = 6;
+            Console.WriteLine("{0} is currently {1} years old", showDogName, pupAge);
+
+
+           
+            string name = "Nellie";
+            Constructor dog = new Constructor(name);
+            Console.WriteLine("My dog's name is {0} and she is a {1} year old Border Collie.", dog.Name, dog.Age);
+            Console.ReadLine();
+        }
+    }
+}
