c4f8734a01791641a160675f4eea34e94e5be61a
vanalsty <64434965+vanalsty@users.noreply.github.com>
2020-07-31 09:04:59 -0700

    FileIOStep219

---
+++ FileIOStep219/FileIOStep219/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOStep219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\lauri\Desktop\C#Projects\Logs\step219.txt", text);
+
+            Console.WriteLine(File.ReadAllText(@"C:\Users\lauri\Desktop\C#Projects\Logs\step219.txt"));
+            Console.ReadLine();
+        }
+    }
+}
